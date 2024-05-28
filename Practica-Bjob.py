38446f028d31de6c6c492ca93c845dcb3f17baa8
desardi131 <desardi131@gmail.com>
2024-05-28 18:51:12 +0200

    V1.0.0 Ejercicio funcional

--- Practica-Bjob.py
+++ Practica-Bjob.py
@@ -9,16 +9,14 @@ class Tarea():
 
     # Constructor con nombre como parametro de entrada, __marcada esta por defecto en False y recoge el momento de crearla
     def __init__(self, nombre:str, momento_creada: Optional[datetime.datetime] = None, marcada: bool = False) -> None: 
+        self.id: int = self.generar_id()
         self.nombre: str = nombre  # Nombre que recibe la tarea
         self._marcada: bool = marcada # nos indica si esta completada o no, por defecto al crear una tarea no estara completada.
         self.__momento_creada: datetime.datetime = momento_creada or datetime.datetime.now() # Momento de crearla, la usaremos para ordenar la lista de tareas
 
     def __str__(self) -> str: # Podemos comprobar el estado de una tarea de manera aislada
-        if not self._marcada:
-            return 'La tarea: {}, No completada, creada en el momento {}'.format(self.nombre,str(self.__momento_creada))
-        else:
-            return 'La tarea: {}, Completada, actualizada en el momento {}'.format(self.nombre,str(self.__momento_creada))
-        
+        estado = 'Completada' if self._marcada else 'No completada'
+        return f'Tarea {self.id}: {self.nombre}, {estado}, creada en el momento {str(self.__momento_creada)}'
     def get_marcada(self) -> bool: # Devuelve el booleano segun este la tarea, por defecto False
         return self._marcada
     
@@ -36,8 +34,34 @@ class Tarea():
     def eliminar(self) -> None:
         tareas = Tarea.cargar_tareas()
         tareas = [tarea for tarea in tareas if tarea['nombre'] != self.nombre] # Realiza un bucle en el que compara el nombre de la tarea en el archivo
+        for idx, tarea in enumerate(tareas):
+            tarea['id'] = idx + 1
+        Tarea.guardar_tareas_dic(tareas)
+    
+    def guardar_tareas(self) -> None:
+        tareas = Tarea.cargar_tareas()
+        for tarea in tareas:
+            if tarea['nombre'].strip().lower() == self.nombre.strip().lower(): # comprueba que la tarea existe y la actualiza
+                tarea['momento_creada'] = self.get_momento_creada()
+                tarea['marcada'] = self.get_marcada()
+                tarea['nombre'] = self.nombre 
+                break
+        else: 
+            tareas.append({ # si la tarea no existe previamente, la crea y la introduce
+                'id': self.id,
+                'nombre': self.nombre,
+                'momento_creada': self.get_momento_creada(),
+                'marcada': self.get_marcada()
+            })
         Tarea.guardar_tareas_dic(tareas)
 
+    @classmethod
+    def generar_id(cls) -> int: # Genera un id a cada tarea que mostraremos al imprimir
+        tareas = cls.cargar_tareas()
+        if not tareas:
+            return 1
+        return max(tarea['id'] for tarea in tareas) +1
+    
     @classmethod
     def buscar_tarea_por_nombre(cls, nombre:str) -> Optional['Tarea']: 
         tareas = cls.cargar_tareas()
@@ -62,22 +86,6 @@ class Tarea():
     def guardar_tareas_dic(cls, tareas: List[dict]) -> None: # Guarda las tareas en el archivo json
         with open(cls.file_path, 'w') as file:
             json.dump(tareas, file, indent=4)
-    
-    def guardar_tareas(self) -> None:
-        tareas = Tarea.cargar_tareas()
-        for tarea in tareas:
-            if tarea['nombre'].strip().lower() == self.nombre.strip().lower(): # comprueba que la tarea existe y la actualiza
-                tarea['momento_creada'] = self.get_momento_creada()
-                tarea['marcada'] = self.get_marcada()
-                tarea['nombre'] = self.nombre 
-                break
-        else: 
-            tareas.append({ # si la tarea no existe previamente, la crea y la introduce
-                'nombre': self.nombre,
-                'momento_creada': self.get_momento_creada(),
-                'marcada': self.get_marcada()
-            })
-        Tarea.guardar_tareas_dic(tareas)
     
 class Consola():
 
@@ -131,10 +139,13 @@ class Consola():
 
     def anadir_tarea(self) -> Tarea:
         nombre: str = input('Introduce una nueva tarea: ') # Se introduce el nombre por consola. siempre sera un string
-        nueva_tarea:Tarea = Tarea(nombre) # Crea la tarea, la cual se introduce en la lista debido a nuestro constructor
-        nueva_tarea.guardar_tareas()
-        print(f'La tarea "{nombre}" ha sido añadida.') # Muestra la tarea recien introducida
-        return nueva_tarea # Devuelve la tarea para poder trabajar con ella, por ejemplo usando los metedos o cambiandole el nombre
+        if nombre == '':
+            print('Por favor, introduce un nombre válido')
+        else:
+            nueva_tarea:Tarea = Tarea(nombre=nombre) # Crea la tarea, la cual se introduce en la lista debido a nuestro constructor
+            nueva_tarea.guardar_tareas()
+            print(f'La tarea "{nombre}" ha sido añadida con ID {nueva_tarea.id}.') # Muestra la tarea recien introducida
+            return nueva_tarea # Devuelve la tarea para poder trabajar con ella, por ejemplo usando los metedos o cambiandole el nombre
 
     # Logica Opcion 2
     def opcion_dos(self) -> None: # Marcado de la tarea 
@@ -186,7 +197,7 @@ class Consola():
                 if tarea_data['nombre'].strip().lower() == nombre.strip().lower():
                     tarea_data['nombre'] = nuevo_nombre
                     tarea.nombre = nuevo_nombre
-                    Tarea.guardar_tareas_dic()
+                    Tarea.guardar_tareas_dic(tareas)
                     print(f'El nombre de la tarea ha sido modificado a "{nuevo_nombre}"')
                     return
         else:
@@ -223,10 +234,8 @@ class Consola():
 ### Funciones ###
 ### -----------------------------------------------------------------------------------------------------------------------------###
 def casilla(tar:Tarea) -> None: # Añade una marca al nombre para indicar si esta completada o no
-    if tar.get_marcada() == False:
-        print(f'[ ] {tar.nombre}')
-    else:
-        print(f'[X] {tar.nombre}')
+    estado = '[X]' if tar.get_marcada() else '[ ]'
+    print(f'{estado} {tar.id}. {tar.nombre}')
 
 def orden_lista(obj_list:List[dict]) -> List[dict]: 
     sorted_obj_list = sorted(obj_list, key=lambda obj: obj['momento_creada']) # Ordena la lista de tareas, en funcion del momento de crearla o modificarla
@@ -238,8 +247,10 @@ def imprimir_lista(): # Carga el archivo Json, toma la lista ordenada de la func
         tarea = Tarea(
             nombre = tarea_data['nombre'],
             momento_creada=datetime.datetime.fromisoformat(tarea_data['momento_creada']),
-            marcada=tarea_data['marcada']
+            marcada=tarea_data['marcada'],
+            
         )
+        tarea.id = tarea_data['id']
         casilla(tarea)
 
 
